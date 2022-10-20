eb7d881a2a53c5813d6422d2c4e5a16643c16e81
deep66366 <78267213+deep66366@users.noreply.github.com>
2022-10-21 00:12:20 +0530

    bug fixed

--- A16_17_Reverse.java
+++ A16_17_Reverse.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
-// caculate Hcf of two numbers in java?
 
 public class A16_17_Reverse {
     public static void main(String args[]) {
         Scanner x = new Scanner(System.in);
-        int m = x.nextInt(),temp;
+        int m = x.nextInt(), temp;
         x.close();
         int rev = 0, sum = 0;
         while (m > 0) {
