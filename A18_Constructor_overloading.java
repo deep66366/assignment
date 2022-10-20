76c5785302dcf09539e8aeb91032bcfc683ed387
deep66366 <78267213+deep66366@users.noreply.github.com>
2022-10-21 00:13:28 +0530

    now again Bug fixed

--- A18_Constructor_overloading.java
+++ A18_Constructor_overloading.java
@@ -1,33 +1,27 @@
 class Shapes {
     double area;
-
     Shapes(float a) {
         area = a * a;
     }
-
     Shapes(float l, float b) {
         area = l * b;
     }
-
     Shapes(float h, double b) {
         area = (h * b) / 2;
     }
-
     Shapes(float l, float b, float h) {
         area = l * b * h;
     }
-
 }
-
 public class A18_Constructor_overloading {
     public static void main(String[] args) {
         Shapes square = new Shapes(5);
-        System.out.println("Area of square is : " + square.area);
-        Shapes rectangle = new Shapes(5.5f, 4);
-        System.out.println("Area of rectangle is : " + rectangle.area);
+        Shapes rectangle = new Shapes(5.5f, 6);
         Shapes triangle = new Shapes(5.2f, 4.5);
-        System.out.println("Area of triangle is : " + triangle.area);
         Shapes cuboid = new Shapes(5.2f, 4.5f, 6);
+        System.out.println("Area of square is : " + square.area);
+        System.out.println("Area of rectangle is : " + rectangle.area);
+        System.out.println("Area of triangle is : " + triangle.area);
         System.out.println("Area of cuboid is : " + cuboid.area);
     }
 }
--- A19_copyConstructor.java
+++ A19_copyConstructor.java
@@ -1,21 +1,23 @@
-class library{
+class library {
     String bname;
     double bprice;
-    library(String name,double price)
-    {
-        bname=name;
-        bprice=price;
+
+    library(String name, double price) {
+        bname = name;
+        bprice = price;
     }
-    library(library lib){
+
+    library(library lib) {
         bname = lib.bname;
         bprice = lib.bprice;
     }
 }
+
 public class A19_copyConstructor {
     public static void main(String[] args) {
         library a = new library("C++", 100);
         library b = new library(a);
-        System.out.println(b.bprice);
-        System.out.println(b.bname);
+        System.out.println("Book name : " + b.bname);
+        System.out.println("Book price : " + b.bprice);
     }
 }
