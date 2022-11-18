a47c778a99f1d5b978589a6e62289500d42f6518
deep66366 <78267213+deep66366@users.noreply.github.com>
2022-11-19 00:53:36 +0530

    modified

--- A38_userdefineecp.java
+++ A38_userdefineecp.java
@@ -1,3 +1,26 @@
+class MyException extends Exception {
+    MyException(String s) {
+        super(s);
+    }
+}
+
 public class A38_userdefineecp {
-    
+
+    static void validate(int age) throws MyException {
+        if (age < 18)
+            throw new MyException("not valid");
+        else
+            System.out.println("welcome to vote");
+    }
+
+    public static void main(String args[]) {
+
+        try {
+            validate(13);
+        } catch (Exception m) {
+            System.out.println("Exception occured: " + m);
+        }
+
+        System.out.println("rest of the code...");
+    }
 }
